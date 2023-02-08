61c8f3d71b3dae36907d136fe2644480992dd1f9
joel <joelzolorza65@gmail.com>
2023-02-08 15:34:30 -0300

    Se agrega la funcionalidad de crear lista de tareas

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const fecha = document.querySelector('#fecha');
 const lista = document.querySelector('#lista');
 const input = document.querySelector('#input');
-const botonEnter = document.querySelector('#boton-enter');
+const botonEnter = document.querySelector('#enter');
 
 
 
 //funcion agregar tarea
 
-function agergarTarea(tarea){
+function agregarTarea(tarea){
     const elemento = ` 
-                    <li>
+                    <li id="elemento">
                         <i class="far fa-circle co" data="realizado" id="0"></i>
                         <p class="text">${tarea}</p>
                         <i class="fas fa-trash de" data="eliminado" id="0"></i>
                     </li>`
 
-    lista.insertAdjacentHTML("beforebegin",elemento);
+    lista.insertAdjacentHTML("beforeend",elemento);
 }   
 
 
@@ -23,7 +23,18 @@ function agergarTarea(tarea){
 botonEnter.addEventListener('click',()=>{
     const tarea = input.value
     if(tarea) {
-        agergarTarea(tarea)
+        agregarTarea(tarea)
     }
-    input.value = '';
+    input.value = ''
 });
+
+
+document.addEventListener('keyup', function (event){
+    if(event.key=='Enter'){
+        const tarea = input.value
+        if(tarea){
+            agregarTarea(tarea)
+        }
+        input.value=''
+    }
+})
