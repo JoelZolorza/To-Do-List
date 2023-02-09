01eefbc71a70863fb2c69e1b416514003315f1bd
joel <joelzolorza65@gmail.com>
2023-02-09 15:38:24 -0300

    Se agrego la Funcion tarea realizada y tarea eliminada

--- script.js
+++ script.js
@@ -2,30 +2,52 @@ const fecha = document.querySelector('#fecha');
 const lista = document.querySelector('#lista');
 const input = document.querySelector('#input');
 const botonEnter = document.querySelector('#enter');
-
-
+const check = 'fa-check-circle';
+const unCheck = 'fa-circle';
+const lineThrough = 'line-through';
+let id = 0;
 
 //funcion agregar tarea
 
-function agregarTarea(tarea){
+function agregarTarea(tarea, id, realizado, eliminado){
+
+    if(eliminado){
+        return  
+    }
+
+    const REALIZADO = realizado ?check :unCheck
+    const LINE = realizado ?lineThrough : ''
+
     const elemento = ` 
                     <li id="elemento">
-                        <i class="far fa-circle co" data="realizado" id="0"></i>
-                        <p class="text">${tarea}</p>
-                        <i class="fas fa-trash de" data="eliminado" id="0"></i>
+                        <i class="far ${REALIZADO} " data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
                     </li>`
 
     lista.insertAdjacentHTML("beforeend",elemento);
-}   
+} 
+
+//funcion tarea Realizada
+
+function tareaRealizda(element){
+    element.classList.toggle(check)
+    element.classList.toggle(unCheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+}
 
 
+
+//Tarea Eliminada
+function tareaEliminada(){}
 
 botonEnter.addEventListener('click',()=>{
     const tarea = input.value
     if(tarea) {
-        agregarTarea(tarea)
+        agregarTarea(tarea, id, false, false)
     }
     input.value = ''
+    id++
 });
 
 
@@ -33,8 +55,23 @@ document.addEventListener('keyup', function (event){
     if(event.key=='Enter'){
         const tarea = input.value
         if(tarea){
-            agregarTarea(tarea)
+            agregarTarea(tarea, id, false, false)
         }
         input.value=''
+        id++
     }
 })
+
+lista.addEventListener('click', function(event){
+    const element = event.target
+    const elementData = element.attributes.data.value
+    if(elementData === 'realizado'){
+        tareaRealizda(element)
+    }
+    else if (elementData === 'eliminado') {
+        tareaEliminada(element)
+    }
+})
+
+
+
